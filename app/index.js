2bd2c977bc5f0e865a42648dd385eec450f15977
Mati <mattiascaricato@gmail.com>
2020-09-20 20:26:44 -0300

    Improve code structure

--- app/index.js
+++ app/index.js
@@ -1,57 +1,56 @@
-window.addEventListener('load', () => {
-  // Wait for loading completion to avoid race conditions with web3 injection timing.
+let web3;
 
-  let web3;
+const connect = async () => {
+  if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
 
-  const connect = async () => {
-    if (window.ethereum) {
-      web3 = new Web3(window.ethereum);
-  
-      try {
-        // Request account access if needed
-        await window.ethereum.enable();
-        console.log('Injected ethereum detected');
-        // Acccounts now exposed
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      // Use Mist/MetaMask's provider.
-      web3 = window.web3;
-      console.log('Injected web3 detected');
-    }
-    // Fallback to localhost; use dev console port by default...
-    else {
-      const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
-      web3 = new Web3(provider);
-      console.log('No web3 instance injected, using Local web3');
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+      console.log('Injected ethereum detected');
+      // Acccounts now exposed
+    } catch (error) {
+      console.error(error);
     }
   }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    // Use Mist/MetaMask's provider.
+    web3 = window.web3;
+    console.log('Injected web3 detected');
+  }
+  // Fallback to localhost; use dev console port by default...
+  else {
+    const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
+    web3 = new Web3(provider);
+    console.log('No web3 instance injected, using Local web3');
+  }
+}
 
+const mint = async (address, amount, contract) => {
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+
+  contract.methods.mint(address, amount).send({ from: account }).then((a) => console.log('TX completed'));
+}
+
+window.addEventListener('load', () => {
+  // Wait for loading completion to avoid race conditions with web3 injection timing.
   connect();
+ });
 
+document.querySelector('.btn-call-to-action').addEventListener('click', () => {
   // Smart Contract for minting DAI
   const contractAdress = '0xFCE4e7ca35CF1Bd56648FC0DBe9285d21C2fA859';
   const abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"usr","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"nonces","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"holder","type":"address"},{"name":"spender","type":"address"},{"name":"nonce","type":"uint256"},{"name":"expiry","type":"uint256"},{"name":"allowed","type":"bool"},{"name":"v","type":"uint8"},{"name":"r","type":"bytes32"},{"name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"usr","type":"address"},{"name":"wad","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"usr","type":"address"},{"name":"wad","type":"uint256"}],"name":"push","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"move","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"usr","type":"address"},{"name":"wad","type":"uint256"}],"name":"pull","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"chainId_","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"}]
   const contract = new web3.eth.Contract(abi, contractAdress);
+  const address = document.querySelector('.flexible-input').value;
+  const amountOfDAI = web3.utils.toWei('1337', 'ether');
 
-  const mint = async (address, amount) => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-
-    contract.methods.mint(address, amount).send({ from: account }).then((a) => console.log('TX completed'));
+  if (!address) {
+    console.error('Address not valid');
+    return;
   }
 
-  document.querySelector('.btn-call-to-action').addEventListener('click', () => {
-    const address = document.querySelector('.flexible-input').value;
-    if (!address) {
-      console.error('Address not valid');
-      return;
-    }
-
-    // Use web3 utils 
-    mint(address, web3.utils.toWei('1337', 'ether'));
-  });
- });
+  mint(address, amountOfDAI, contract);
+});
