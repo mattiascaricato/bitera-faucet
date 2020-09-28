f081679fae10d572e6eebee1b04e861bd5394178
Mati <mattiascaricato@gmail.com>
2020-09-28 20:47:11 -0300

    Use web3 CDN

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ const app = express();
 const port = 3000;
 
 // Server static files from app folder
-app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static(path.join(__dirname, '../app')));
 app.use(compression());
 
 // Serve web3 js from node_modules
-app.get('/web3.min.js', function(req, res) {
-  res.sendFile(`${__dirname}/node_modules/web3/dist/web3.min.js`);
-});
+//app.get('/web3.min.js', function(req, res) {
+//  res.sendFile(`${__dirname}/node_modules/web3/dist/web3.min.js`);
+//});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
