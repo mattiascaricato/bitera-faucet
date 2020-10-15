2c671d2e1fa5fb5ab1bcf9cd0da4126f8b6bac90
Mati <mattiascaricato@gmail.com>
2020-10-14 23:35:27 -0300

    Add mocked state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+import { useState } from 'preact/hooks';
 import './style.css';
 
 const Title = () => (
   <h1>Bitera Faucet</h1>
 );
 
-const AddressInput = () => (
-  <input type="text" placeholder="Ingresa la dirección de tu testnet wallet de Bitera"></input>
+const AddressInput = ({ value, onChange }) => (
+  <input type="text" value={value} onChange={onChange} placeholder="Ingresa la dirección de tu testnet wallet de Bitera"></input>
 );
 
 const SubmitButton = () => (
@@ -16,10 +17,10 @@ const Disclaimer = () => (
   <p class="disclaimer">* DAI de la red de Ethereum Ropsten, <strong>NO es DAI real</strong></p>
 );
 
-const FaucetForm = () => (
-  <form>
+const FaucetForm = ({ value, onChange, onSubmit }) => (
+  <form onSubmit={onSubmit}>
     <Title />
-    <AddressInput />
+    <AddressInput value={value} onChange={onChange} />
     <SubmitButton />
     <Disclaimer />
   </form>
@@ -34,34 +35,49 @@ const Copyright = () => (
 );
 
 const AccountHeader = (props) => {
-  const { account: { address }} = props;
+  const { account: { address }, onClick } = props;
 
-  const shortAddress = `${address.substring(0, 6)}...${address.substring(address.length - 4, address.length)}`;
+  const getShortAddress = (address) => `${address.substring(0, 6)}...${address.substring(address.length - 4, address.length)}`;
 
   return (
     address
-    ? <button>{shortAddress}</button>
-    : <button>Conectá tu Wallet <span></span></button>
+      ? <button onClick={onClick}>{getShortAddress(address)}</button>
+      : <button onClick={onClick}>Conectá tu Wallet <span></span></button>
   );
 };
 
-export default () => (
+export default () => {
+  const [account, setAccount] = useState({ address: '' });
+  const [toAddress, setToAddress] = useState('');
+
+  const handleAccountOnClick = () => {
+    account.address
+      ? setAccount(prevState => ({ ...prevState, address: '' }))
+      : setAccount(prevState => ({ ...prevState, address: '0x8b2db6397De65D4C03429BC2e7f09515f98AC214' }));
+  }
+
+  const handleAdressChange = (e) => {
+    setToAddress(e.target.value);
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
   <div class="container">
     <header class="header">
-      <AccountHeader account={ACCOUNT}/>
+      <AccountHeader onClick={handleAccountOnClick} account={account}/>
     </header>
     <div class="logo">
       <Logo />
     </div>
     <section class="content">
-      <FaucetForm />
+      <FaucetForm value={toAddress} onChange={handleAdressChange} onSubmit={handleFormSubmit} />
     </section>
     <footer class="footer">
       <Copyright />
     </footer>
   </div>
-);
-
-const ACCOUNT = {
-  address: '0x8b2db6397De65D4C03429BC2e7f09515f98AC214'
-};
+  )
+}
